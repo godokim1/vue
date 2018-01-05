d938664e54079a363b499856213dbc8d369799bc
gimminsu <godokim1@naver.com>
2018-01-05 16:19:40 +0900

    브랜치 테스트

---
+++ components/img_text.vue
@@ -0,0 +1,36 @@
+
+<template>
+  <div class="well">
+    <form class="form-horizontal" role="form">
+      <h4>What's New</h4>
+      <div class="form-group" style="padding:14px;">
+        <textarea class="form-control" placeholder="Update your status" v-model="msg" v-on:keyup.enter="submit"></textarea>
+      </div>
+      <button type="button" @click="testing1">글쓰기</button>
+    </form>
+  </div>
+</template>
+
+
+<script>
+// import Test from "@/components/test";
+
+export default {
+  // name: 'default_text',
+  created() {
+    console.log(localStorage);
+    localStorage.test = 1;
+  },
+  data() {
+    return {
+      msg: localStorage.item,
+    };
+  },
+  methods: {
+    testing1() {
+      localStorage.item = this.msg;
+      return false;
+    },
+  },
+};
+</script>
